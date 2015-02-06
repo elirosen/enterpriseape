0b192e8f97ac2989a5ef17bcaa9d13ddc2d0157a
Eli Rosen <erosen34@gmail.com>
2015-02-06 18:24:03 +0000

    added host and set consider_all_requsts_local = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -82,5 +82,6 @@ Rails.application.configure do
   #commenting this out based on jordans code. 
   #config.active_record.dump_schema_after_migration = false
   
+  config.action_mailer.default_url_options = { :host => "erosen34enterpriseape1.herokuapp.com" }
 
 end
